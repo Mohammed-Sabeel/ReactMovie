e9ed3bebaa5ea1705df9a76a057df952c6629bbc
Sabeel <sabeel9965@gmail.com>
2024-02-18 19:02:43 +0530

    commited files

---
+++ src/Datas.js
@@ -0,0 +1,15 @@
+export const movieData = [
+   
+    {
+        title:"Beast",
+        description:"Beast is the latest Tamil action-movie featuring Atharvaa Murali, Tanya Ravichandran in the main roles.",
+        image:"https://i0.wp.com/www.newsbugz.com/wp-content/uploads/2022/04/Beast-Mode.jpg?fit=1280%2C720&ssl=1"
+    },
+    {
+        title:"KGF",
+        description:"KGF is the latest Kanada action-movie featuring Atharvaa Murali, Tanya Ravichandran in the main roles.",
+        image:"https://static.toiimg.com/thumb/msid-91740611,width-1280,height-720,resizemode-4/.jpg"
+    },
+    
+  
+]
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MovieProvider } from './Context/moiveContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <MovieProvider>
+    <Router>
+      <App />
+
+    </Router>
+  </MovieProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
